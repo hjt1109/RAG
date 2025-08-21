a785c40cd5ccaeacbb95c94bddfcce3094b6f4e2
元素 <maxingwang1990@126.com>
2025-08-21 09:42:24 +0800

    测试文件:根据collection_name指定向量数据库表

---
+++ app/TestMilvus.py
@@ -0,0 +1,26 @@
+from unittest import result
+from numpy.random import f
+from pymilvus.orm import collection
+from milvus_utils import My_MilvusClient
+
+mc = My_MilvusClient(dim = 128, collection_name="test_collection")
+class TestMilvus:
+    def __init__(self) :
+        self.dim = 128
+        self.collection_name = "test_collection"
+
+    
+    def searchinMy_MilvusClient(self, top_k):
+        results = mc.client.search(
+            collection_name = "test_collection",
+            data = [[1.0]*self.dim],
+            limit=top_k,
+            output_fields = ["e"]
+        )
+        return results[0]
+
+
+
+if __name__ == '__main__':
+    test = TestMilvus()
+    print(test.searchinMy_MilvusClient(1))
